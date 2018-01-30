6bca862b31c9673581dd90739331bf6416697bab
Austin Cornelio <austin.cornelio@mastercard.com>
2018-01-30 15:24:31 -0500

    finished scale and pair program session

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    public String WELCOME_MESSAGE;
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return WELCOME_MESSAGE;
     }
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message) {
+        WELCOME_MESSAGE = message;
+    }
+
 }
